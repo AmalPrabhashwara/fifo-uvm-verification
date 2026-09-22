e990d96f20b864137f2b7025d2360a387a2c6b52
amal <prabhashwaramal@gmial.com>
2026-09-22 12:17:04 +0530

    rtl source files

---
+++ rtl/fifo.sv
@@ -0,0 +1,39 @@
+module fifo
+#(
+    parameter DEPTH=16,
+    parameter WIDTH=8
+)(
+    input logic clk,rst,
+    input logic [WIDTH-1:0]din,
+    input logic wen,ren,
+    output logic [WIDTH-1:0]dout,
+    output logic empty,full
+);
+logic [WIDTH-1:0] mem [0:DEPTH-1];//unpacked array
+logic [$clog2(DEPTH):0] wCnt;
+logic [$clog2(DEPTH):0] rCnt;
+
+always_ff @(posedge clk)begin
+    if(rst)begin
+        wCnt<=0;
+        rCnt<=0;
+        for(int i=0; i<DEPTH; i++)begin
+            mem[i]<=0;
+        end
+    end
+    else begin
+      if(!full && wen)begin
+            mem[wCnt[$clog2(DEPTH)-1:0]]<=din;
+            wCnt<=wCnt+1;
+        end
+      if(!empty && ren)begin
+            dout<=mem[rCnt[$clog2(DEPTH)-1:0]];
+            rCnt<=rCnt+1;
+        end
+    end
+end
+
+assign full=(wCnt[$clog2(DEPTH)]!=rCnt[$clog2(DEPTH)])&&(wCnt[$clog2(DEPTH)-1:0]==rCnt[$clog2(DEPTH)-1:0]);
+assign empty=(wCnt==rCnt);
+
+endmodule
